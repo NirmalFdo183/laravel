368139908116fc5461459cc62a164b26e78b4433
Nirmal Fernando <fernandonirmal345@gmail.com>
2026-01-20 18:50:57 +0530

    CRUD todo app project set up

---
+++ CRUD/frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className='ui fixed inverted menu'>
+        <div className='ui container'>
+          <a href="/#" className='header item'>React Js CRUD laravel API</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
