60bf7a8d145c2aab6d15815bb0c671e71dad7c57
Nirmal Fernando <fernandonirmal345@gmail.com>
2026-01-23 23:20:14 +0530

    updating the user model

--- TravelAgency/backend/app/Models/User.php
+++ TravelAgency/backend/app/Models/User.php
@@ -21,6 +21,12 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
+        'address',
+        'phone',
+        'avatar',
+        'city',
+        'is_verified',
     ];
 
     /**
@@ -44,5 +50,13 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function isGuide(){
+        return $this->role === 'guide';
+    }
+
+    public function isTourist(){
+        return $this->role === 'tourist';
     }
 }
