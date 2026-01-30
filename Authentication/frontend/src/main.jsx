3a64caa20d30bb077db77751341a9947e3566155
Nirmal Fernando <fernandonirmal345@gmail.com>
2026-01-30 20:50:24 +0530

    feat: add routing with react-router and create Login page

--- Authentication/frontend/src/main.jsx
+++ Authentication/frontend/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
+import React from 'react'
+import { RouterProvider } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { router } from './routes/index.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
 )
