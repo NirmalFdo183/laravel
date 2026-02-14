6ba7207a44e99c7031d47d62cd5c00a26f2e3d0b
Nirmal Fernando <fernandonirmal345@gmail.com>
2026-02-14 13:48:56 +0530

    deleteing front end poart for authentication and building a monolothic project

--- Authentication/frontend/src/routes/index.jsx
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter } from "react-router-dom";
-import Login from "../pages/Login";
-
-export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Login/>
-    },
-]);
